a4539987dd1765700e899d094aea07311018c5e4
Javier Florez <javier_florezg@hotmail.com>
2025-06-26 18:36:08 -0500

    Primer Lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TercerRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Javier Florez",                         # Tu nombre
+    author_email="javier_florezg@hotmail.com",      # Tu correo electrónico
+    url="https://github.com/jflorez77/TercerRepo",     # URL del proyecto
+)
